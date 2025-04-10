6f188391979926fd2b1149e557fe293114f8ae07
Miruto80 <miguelfernando8012@gmail.com>
2025-04-10 10:41:19 -0400

    Subiendo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let currentImageIndex = 0;
 const images = [
+    "imagenes/Last adds/Last add (5).jpeg",
     "imagenes/Last adds/Last add (1).jpeg",
     "imagenes/Last adds/Last add (2).jpeg",
     "imagenes/Last adds/Last add (3).jpeg"
