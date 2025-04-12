e687f038f3f0baba6de2fbb6a9bd8f84758d3446
Miruto80 <miguelfernando8012@gmail.com>
2025-04-12 11:31:59 -0400

    Eliminando

--- Inventory.php
+++ Inventory.php
@@ -772,34 +772,7 @@
                 <button onclick="openForm('2017 Infiniti QX60')">Request Information</button>
             </div>
         </div>
-        <div class="car">
-            <div class="car-images">
-            <img src="imagenes/Suv/2007 Jeep Wrangler UNLIMITEDX (1).jpeg" title="2007 Jeep Wrangler UNLIMITEDX" alt="2007 Jeep Wrangler UNLIMITEDX">
-            <img src="imagenes/Suv/2007 Jeep Wrangler UNLIMITEDX (2).jpeg" title="2007 Jeep Wrangler UNLIMITEDX" alt="2007 Jeep Wrangler UNLIMITEDX">
-            <img src="imagenes/Suv/2007 Jeep Wrangler UNLIMITEDX (3).jpeg" title="2007 Jeep Wrangler UNLIMITEDX" alt="2007 Jeep Wrangler UNLIMITEDX">
-            <img src="imagenes/Suv/2007 Jeep Wrangler UNLIMITEDX (4).jpeg" title="2007 Jeep Wrangler UNLIMITEDX" alt="2007 Jeep Wrangler UNLIMITEDX">
-            <img src="imagenes/Suv/2007 Jeep Wrangler UNLIMITEDX (5).jpeg" title="2007 Jeep Wrangler UNLIMITEDX" alt="2007 Jeep Wrangler UNLIMITEDX">
-            <img src="imagenes/Suv/2007 Jeep Wrangler UNLIMITEDX (6).jpeg" title="2007 Jeep Wrangler UNLIMITEDX" alt="2007 Jeep Wrangler UNLIMITEDX">
-        </div>
-        
-            <div class="car-info">
-                <div class="car-controls">
-                    <button class="arrow" onclick="prevImage(this)">&#9664;</button>
-                    <button class="arrow" onclick="nextImage(this)">&#9654;</button>
-               </div>
-                <h3>2007 Jeep Wrangler</h3>
-                <div>
-                    <p><b>Miles:</b> 169,811</p>
-                    <p><b>Cash:</b> $8.300</p>
-                    <button class="more-info-btn" onclick="toggleMoreInfo(this)">Show details &#9660;</button>
-                    <div class="more-info">
-                    <p><b>Financing:</b>$12.500  (Negotiable)</p> <!-- Garantia incluida-->
-                    <p><b>Down:</b> $3.000 (Negotiable)</p>
-                </div>
-                </div>
-                <button onclick="openForm('2007 Jeep Wrangler UNLIMITEDX')">Request Information</button>
-            </div>
-        </div>
+       
         <!--More suvs here-->
     </div>
     <hr>
