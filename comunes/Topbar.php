75f0b2081c2d8d562a6adbc903e6cba57eca9505
Miruto80 <miguelfernando8012@gmail.com>
2026-01-08 12:41:33 -0400

    quitando facebook

--- comunes/Topbar.php
+++ comunes/Topbar.php
@@ -41,7 +41,7 @@
         <div class="top-bar-content">
             <span class="contact-info"><i class="fa-solid fa-phone"> Call us:</i><a href="tel:469-275-6620"> 469-275-6620</a></span>
             <div class="social-links">
-                <a href="https://www.facebook.com/profile.php?id=61572580624185" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.facebook.com/share/1BgkwLX8qh/?mibextid=wwXIfr" target="_blank"><i class="fa-brands fa-facebook"></i></a>
                 <a href="https://www.instagram.com/usedcarsforsaletxfl?igsh=MWZ6c3IyOW5zODBkdA%3D%3D&utm_source=qr" target="_blank"><i class="fa-brands fa-instagram"></i></a>
             </div>
         </div>
