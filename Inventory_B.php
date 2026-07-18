9c6b6fdb3a49338a85e9e5e19076317d10cdcd7e
Miruto80 <miguelfernando8012@gmail.com>
2026-07-18 14:01:59 -0400

    Seccion nueva

--- Inventory_B.php
+++ Inventory_B.php
@@ -10,7 +10,7 @@
 <?php require_once("comunes/Ws.php")?>
 
 <div class="title-select-container">
-    <h1 id="header">Car Inventory</h1>
+    <h1 id="header">Bank financing cars</h1>
 </div>
 
 <div class="container mt-5">
